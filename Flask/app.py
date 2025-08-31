be37be8ac74223d59fd92f831f2f8aefde4ec8e7
havebeen4@naver.com <havebeen4@naver.com>
2025-09-01 00:13:28 +0900

    웹개발 3주차: 0831 Flask로 영화 검색 사이트 만들기, 3주차 숙제

--- Flask/app.py
+++ Flask/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
+import requests
 app = Flask(__name__)
 
 @app.route('/')
@@ -31,6 +32,31 @@ def home():
 @app.route('/mypage')
 def mypage():
     return 'This is Mypage!'
+
+@app.route('/movie')
+def movie():
+    query = request.args.get('query')
+    res = requests.get(
+	f"http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=6b3423156bb0fe131e9d7242b01d0234&movieNm={query}"
+    )
+    rjson = res.json()
+    movie_list = rjson["movieListResult"]["movieList"]
+    return render_template('movie.html', data=movie_list)
+
+@app.route("/answer")
+def answer():
+    if request.args.get('query'):
+        query = request.args.get('query')
+    else:
+        query = '20230601'    
+
+    URL = f"http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=6b3423156bb0fe131e9d7242b01d0234&targetDt={query}"
+
+    res = requests.get(URL)
+    rjson = res.json()
+    print(rjson)
+    boxoffice_list = rjson["boxOfficeResult"]["weeklyBoxOfficeList"]
+    return render_template("answer.html", data=boxoffice_list)
 
 if __name__ == '__main__':  
     app.run(debug=True)
